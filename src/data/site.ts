06cf991e16eec322ce0bce18c7561c82a263fd51
parthu7990 <parth20098@gmail.com>
2026-08-08 09:37:12 +0530

    Changes of link

--- src/data/site.ts
+++ src/data/site.ts
@@ -14,7 +14,7 @@ export const siteConfig = {
   linkedin: 'https://www.linkedin.com/in/parth-prajapati-257b66325',
   instagram: 'https://www.instagram.com/parth_.7990',
   twitter: 'https://twitter.com/parthprajapati',
-  resumeUrl: 'https://drive.google.com/file/d/1fqY-N0K_02GmMND1oqFY4goZOqS0H9T1/view?usp=drive_link',
+  resumeUrl: 'https://drive.google.com/file/d/162fjbN5uMgJ5OBFYN-BmfuLWlPFclEEb/view?usp=drive_link',
   available: true,
 };
 
